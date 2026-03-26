ddfcd198136d5c01e3bee41121216a9479850d6b
makhmudovs <makhmudovs50@gmail.com>
2026-03-26 14:50:59 +0800

    better-auth auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,9 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/app/db/index"; // your drizzle instance
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "mysql", "sqlite"
-    }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  database: drizzleAdapter(db, {
+    provider: "pg", // or "mysql", "sqlite",
+    
+  }),
+   plugins: [nextCookies()] 
 });
